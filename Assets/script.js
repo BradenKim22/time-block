ff332a0cfa89a3dbcecd35401439ba5f686f3746
BradenKim22 <118400033+BradenKim22@users.noreply.github.com>
2023-01-26 17:29:24 -0700

    Got the blocks and the color

--- Assets/script.js
+++ Assets/script.js
@@ -2,14 +2,71 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // variables for the date in header
+  // variables to interact with the DOM
   var currentDay = $('#currentDay');
-  // variable current, day, and date
+  var container = $('.container-lg');
+
+  // variable current, +time, day, and date
   var today = dayjs();
   var dayWeek = today.format('dddd');
   var currentDate = today.format("MMMM D");
-  // variable for the container
-  var containerLg = $('.container-lg');
+
+  // variables for the work day schedule // for loop create time block with all classes, and buttons, etc.
+  var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  // functions for the time block color
+  function past() {
+    timeBlock.addClass('class', 'past');
+  };
+  function present() {
+    timeBlock.addClass('class', 'present');
+  };
+  function future() {
+    timeBlock.addClass('class', 'future');
+  };
+
+  for (var i = 0; i < workHours.length; i++) {
+    // The Row in the container variables
+    var timeBlock = $('<div>');
+    // Part 1 of the Row, the hour variables
+    var theHour = $('<div>');
+    var hour = dayjs().hour(workHours[i]).format('hA');
+    var nowHour = dayjs().hour();
+    // Part 2 of the Row, the textarea variables
+    var textArea = $('<textarea>');
+    // Part 3 of the Row, the button variables and the save icon
+    var button = $('<button>');
+    var buttonI = $('<i>');
+
+    // Row Div which will be in 3 parts
+    timeBlock.attr('id', workHours[i]);
+    timeBlock.attr('class', 'row time-block');
+    // Lets do the colors in the loop too
+    if (nowHour > workHours[i]) {
+      timeBlock.addClass('past');
+    } else if (nowHour === workHours[i]) {
+      timeBlock.addClass('present');
+    } else {
+      timeBlock.addClass('future');
+    };
+    // Part 1 of the row, the time
+    theHour.addClass('col-2 col-md-1 hour text-center py-3');
+    theHour.html(hour);
+    // Part 2 of the row, the textarea
+    textArea.addClass('col-8 col-md-10 description');
+    // textArea.attr('row', workHours.length);
+    // Part 3 of the row, the button. with the Save Icon on the Button
+    button.addClass('btn saveBtn col-2 col-md-1');
+    button.attr('aria-label', 'save');
+    buttonI.addClass('fas fa-save');
+    buttonI.attr('aria-hidden', 'true');
+
+    // Append the row to the container then append all three parts to the row.
+    container.append(timeBlock);
+    timeBlock.append(theHour);
+    timeBlock.append(textArea);
+    timeBlock.append(button);
+    button.append(buttonI);
+  };
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
